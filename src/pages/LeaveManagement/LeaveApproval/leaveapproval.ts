4c616be4ef807c1a7970cccd1f7e7d056b13ebed
Mahesh Nikate <maheshnikate@Mahesh-MAC.local>
2017-02-06 04:19:49 -0800

    Leave Approval and Holiday page UI.

--- src/pages/LeaveManagement/LeaveApproval/leaveapproval.ts
+++ src/pages/LeaveManagement/LeaveApproval/leaveapproval.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController,Alert,ItemSliding,AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-leaveapproval',
@@ -8,8 +8,66 @@ import { NavController } from 'ionic-angular';
 })
 export class LeaveApproval {
 
-  constructor(public navCtrl: NavController) {
-    
+public isShowMyLeave:boolean;
+public isSelectall:boolean;
+public leavechecked: boolean;
+
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+    this.isShowMyLeave = false;
+    this.isSelectall = false;
+    this.leavechecked = false;
   }
+  selectAllLeaves()
+  {
+    this.isSelectall = !this.isSelectall;
+    
+    if(this.isSelectall == true)
+    this.leavechecked = true;
+  }
+  approveLeave()
+  {
+    this.showApproveRejectPromt(true);
+  }
+  rejectLeave()
+  {
+    this.showApproveRejectPromt(false);
+  }
+  showApproveRejectPromt(approve:boolean)
+  {
+    var isApprove: String = "Approve";
+    if(approve)
+    isApprove = "Approve";
+    else
+    isApprove = "Reject";
+
+let prompt = this.alertCtrl.create({
+      title: 'Leave',
+      message: "Enter a comment for Leave!",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Comment'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: isApprove,
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+  
 
 }
